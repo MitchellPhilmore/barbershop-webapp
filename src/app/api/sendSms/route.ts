4ab62389495e46ae5d86035a22e2870612e9d438
Mitch <Mitchellphilmore@gmail.com>
2024-04-06 14:12:33 -0400

    fix sms route by adding correct type for request param

--- src/app/api/sendSms/route.ts
+++ src/app/api/sendSms/route.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from 'next/server';
 import twilio from 'twilio';
 
-export async function POST (req:Response) {
+export async function POST (req:Request,res:Response) {
     const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
     const {message, subject, phoneNumber} =  await req.json()
+    console.log(message,subject,phoneNumber)
     console.log(message, phoneNumber, process.env.TWILIO_PHONE_NUMBER)
     try {
       const txtMessage = await client.messages.create({
