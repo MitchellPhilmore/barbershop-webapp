4479902bfa1c4579bcc78df0b66dca35ba7da5ca
Mitch <Mitchellphilmore@gmail.com>
2024-04-06 13:49:15 -0400

    add keys to images

--- src/app/components/testimonial.tsx
+++ src/app/components/testimonial.tsx
@@ -18,18 +18,19 @@ export const TestimonialCard = ({
   return (
     <div className=" bg-white shadow-lg flex flex-col w-full">
       <div className="flex cursor-pointer">
-        <img src={imageUrl} alt="Card" className="w-1/3 object-cover" />
+        <img
+          key={name + 1}
+          src={imageUrl}
+          alt="Card"
+          className="w-1/3 object-cover"
+        />
         <div className="flex-1 p-4">
           <h2 className="text-xl text-center font-black ">{title}</h2>
-          <p className="text-sm text-center my-2">
-            {description}
-          </p>
+          <p className="text-sm text-center my-2">{description}</p>
         </div>
       </div>
       <div className="mt-auto p-4 bg-white flex justify-end items-center capitalize">
-        <span className="text-black text-xl tracking-wider ">
-          {name}
-        </span>
+        <span className="text-black text-xl tracking-wider ">{name}</span>
         <span className="bg-gray-500 h-full w-px mx-2"></span>
         <span className=" text-gray-400 capitalize">{location}</span>
       </div>
